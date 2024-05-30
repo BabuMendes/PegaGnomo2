73e3de168328f324b5195ee356e5867235239d6d
BabuMendes <gmendesdEsouza345@gmail.com>
2024-05-29 21:41:28 -0300

    Sound
    Sound

---
+++ Pega Gnomo/Assets/Scripts/Timer.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private GerenciadorDoJogo gerenciadorDoJogo;
+
+    [SerializeField] private TMP_Text timerTxt;
+
+    private float min;
+    private float sec;
+    private bool isGameOver;
+
+    void Start()
+    {
+        isGameOver = false;
+        SetTimer(0, 45);
+    }
+
+    void Update()
+    {
+        if (!isGameOver && gerenciadorDoJogo.jogoON)
+        {
+            CountTime();
+        }
+    }
+
+    public void SetTimer(float min, float sec)
+    {
+        this.min = min;
+        this.sec = sec;
+    }
+
+    private void CountTime()
+    {
+        if (min >= 0)
+        {
+            sec -= 1 * Time.deltaTime;
+
+            if (sec <= 0)
+            {
+                min -= 1;
+                sec = 59;
+            }
+        }
+
+        if (min < 0)
+        {
+            min = 0;
+            sec = 0;
+            isGameOver = true;
+            GameOver();
+            //Time.timeScale = 0f;
+        }
+
+        if (sec >= 9.5f)
+        {
+            timerTxt.SetText("0" + min + ":" + Mathf.Round(sec));
+        }
+        else
+        {
+            timerTxt.SetText("0" + min + ":0" + Mathf.Round(sec));
+        }
+    }
+
+    private void GameOver()
+    {
+        gerenciadorDoJogo.Morreu();
+    }
+}
